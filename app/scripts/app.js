5a5bc97d844f3155ea96d193c67b38a398e9233c
Owen Davies <owen@odavi.es>
2015-09-26 14:17:14 +0100

    Add scaffolding
    Node
    Angular
    Grunt
    Karma
    Probably more shit I don't know about

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name diddleplanApp
+ * @description
+ * # diddleplanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('diddleplanApp', []);
