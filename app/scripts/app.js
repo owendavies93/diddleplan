2192b6937711ca53cc3234b43bbd7554d6a4a309
Anna Thomas <annathom@uk.ibm.com>
2015-09-27 15:27:03 +0100

    Use angular-ui-router for routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,22 @@
  * Main module of the application.
  */
 angular
-  .module('diddleplanApp', []);
+  .module('diddleplanApp', [
+    'ui.router'
+  ])
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    //
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise("/");
+    //
+    // Now set up the states
+    $stateProvider
+      .state('calendar', {
+        url: "/",
+        templateUrl: "/views/calendar.html",
+        controller: "CalendarCtrl"
+      });
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+  });
