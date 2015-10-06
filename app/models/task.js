73665ec59f09743c29f797cb826418e68ffd884e
Owen Davies <owen@odavi.es>
2015-10-06 18:14:34 +0100

    Use more consistent attribute naming

--- app/models/task.js
+++ app/models/task.js
@@ -2,11 +2,12 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Task = sequelize.define("Task", {
-    name:          { type: DataTypes.STRING, allowNull: false },
-    task_type:     { type: DataTypes.ENUM('todo', 'exercise', 'meal', 'meeting', 'other'), allowNull: false, defaultValue: 'other' },
-    date:          { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
-    moveable:      { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: 1 },
-    auto_moveable: { type: DataTypes.BOOLEAN, allowNull: true, defaultValue: null }
+    taskID:       { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    name:         { type: DataTypes.STRING, allowNull: false },
+    taskType:     { type: DataTypes.ENUM('todo', 'exercise', 'meal', 'meeting', 'other'), allowNull: false, defaultValue: 'other' },
+    date:         { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    moveable:     { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: 1 },
+    autoMoveable: { type: DataTypes.BOOLEAN, allowNull: true, defaultValue: null }
   }, {
     classMethods: {
       associate: function(models) {
