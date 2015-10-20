dfc62258413e067f98811f90210678b3fb42d456
Owen Davies <owen@odavi.es>
2015-10-20 11:09:06 +0100

    Remove some debug output

--- app/routes/web.js
+++ app/routes/web.js
@@ -5,7 +5,6 @@ var path    = require('path');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
-  console.log(req.user);
   res.sendFile(path.resolve('app/index.html'));
 });
 
